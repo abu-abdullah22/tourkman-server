e90bb2793f2d778a7070b64ee732ec93e33e61cc
abu-abdullah22 <rorschachmayhem@gmail.com>
2024-04-26 23:22:11 +0600

    post and read data done for tourist section

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(express.json()) ;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gvqow0e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-console.log(uri);
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,12 +28,30 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const spotCollection = client.db('spotDB').collection('spots') ;
+
+    app.post('/spots', async (req,res)=> {
+      const spotAdded = req.body ;
+      console.log(spotAdded);
+      const result = await spotCollection.insertOne(spotAdded) ;
+      res.send(result) ;
+     })
+
+     app.get('/spots', async(req,res)=> {
+      const cursor = spotCollection.find() ;
+      const result = await cursor.toArray() ;
+      res.send(result) ;
+     })
+
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
